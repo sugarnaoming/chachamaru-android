16a325a9baff6bdb33aa8115174469db234b6734
sugarnaoming <sakigithub@gmail.com>
2018-01-10 16:49:12 +0900

    java/.../MainActivity: メインメニューの動的生成を追加

--- app/src/main/java/com/example/sugarnaoming/chachamaru/MainActivity.kt
+++ app/src/main/java/com/example/sugarnaoming/chachamaru/MainActivity.kt
@@ -1,29 +1,51 @@
 package com.example.sugarnaoming.chachamaru
 
+import android.content.DialogInterface
 import android.os.Bundle
-import android.support.design.widget.Snackbar
+import android.provider.ContactsContract
 import android.support.design.widget.NavigationView
+import android.support.v4.app.FragmentManager
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import com.example.sugarnaoming.chachamaru.datamodel.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import java.io.Serializable
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+  lateinit var fragmentManager:FragmentManager
+  lateinit var dbController: DatabaseController
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     setSupportActionBar(toolbar)
+    this.fragmentManager = supportFragmentManager
 
+    val leftMenu = findViewById<NavigationView>(R.id.nav_view).menu
+
+    this.dbController = DatabaseController(applicationContext)
+    val menuTitles = dbController.getGroupNamesWhereUrlExists()
+    // menuに動的追加する
+    menuTitles.forEachIndexed { index, groupName ->
+      // p2の数値(index)が順番を表している。これを入れないと表示される順番が保証されない。
+      leftMenu.add(R.id.left_menu_group, Menu.NONE, index, groupName)
+    }
     val toggle = ActionBarDrawerToggle(
         this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
     drawer_layout.addDrawerListener(toggle)
     toggle.syncState()
-
     nav_view.setNavigationItemSelectedListener(this)
+    try {
+      firstViewFragment(UrlsList(dbController.getUrlsByGroupNameOf(menuTitles.first())))
+    }catch (e: BadRequestException) { createExceptionDialog(e)
+    }catch (e: Exception) {createExceptionDialog(e) }
   }
 
   override fun onBackPressed() {
