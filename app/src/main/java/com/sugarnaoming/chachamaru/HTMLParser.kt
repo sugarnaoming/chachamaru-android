bc7580f16b8a8700f95b49a7434127a5ed13a550
sugarnaoming <sakigithub@gmail.com>
2018-01-16 20:33:09 +0900

    chachamaru/HTMLParser: HTMLパース用のクラスを追加

---
+++ app/src/main/java/com/sugarnaoming/chachamaru/HTMLParser.kt
@@ -0,0 +1,18 @@
+package com.sugarnaoming.chachamaru
+
+import org.jsoup.Jsoup
+
+class HTMLParser {
+  fun removeImgTag(targetHtml: String): String {
+    val html = "$HTML_PREFIX$targetHtml$HTML_SUFFIX"
+    val document = Jsoup.parse(html)
+    document.getElementsByTag("img").remove()
+    document.getElementsByTag("a")
+    val documentBodyHtml = document.body().toString()
+    return documentBodyHtml.replaceFirst("<body>","").reversed().replaceFirst(">ydob/<", "").reversed()
+  }
+  companion object {
+    private val HTML_PREFIX = "<html><head></head><body>"
+    private val HTML_SUFFIX = "</body></html>"
+  }
+}
