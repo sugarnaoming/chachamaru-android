a3dfbad4cf9c2a2550f2681d4e1af70b8a43da8e
sugarnaoming <sakigithub@gmail.com>
2018-01-24 17:22:12 +0900

    DB設定用クラスを追加、DBからグループ全件取得用メソッドの追加

---
+++ app/src/main/java/com/sugarnaoming/chachamaru/ConfigDatabase.kt
@@ -0,0 +1,51 @@
+package com.sugarnaoming.chachamaru
+
+class ConfigDatabase {
+  val invokeForeignKeySql: String
+    get() = "PRAGMA foreign_keys = ON"
+  val getSqlCreateGroupListTable: String
+    get() = "create table $TABLE_NAME_GROUPLIST(" +
+        "_id integer primary key autoincrement, " +
+        "group_name text not null unique)"
+  val getSqlCreateUrlListTable: String
+    get() = "create table $TABLE_NAME_URLLIST(" +
+        "_id integer primary key autoincrement," +
+        "title text not null," +
+        "group_name text not null," +
+        "url text not null," +
+        "is_rss boolean not null," +
+        "unique(title, group_name)," +
+        "foreign key(group_name) references $TABLE_NAME_GROUPLIST(group_name))"
+  val getSqlDefaultInsertToGroupList: String
+    get() = "insert into $TABLE_NAME_GROUPLIST(group_name)" +
+        "values('はてなブックマーク/新着')," +
+        "('はてなブックマーク/人気')," +
+        "('Qiita')," +
+        "('Web')," +
+        "('情報全般')"
+  val getSqlDefaultInsertToUrlList: String
+    get() {
+      val hatebuEntryBaseUrl = "http://b.hatena.ne.jp/entrylist"
+      val hatebuHotBaseUrl = "http://b.hatena.ne.jp/hotentry"
+      val qiitaBaseUrl = "api/qiita/rank"
+      val suffix = ".rss"
+      val rssTrue = 1
+      val rssFalse = 0
+      val categoriesName = listOf("", "/social", "/economics", "/life", "/knowledge", "/it", "/entertainment", "/game", "/fun")
+      val categories = categoriesName.map { "$it$suffix" }
+      return "insert into urllist(title, group_name, url, is_rss) " +
+          "values('総合', 'はてなブックマーク/新着', '$hatebuEntryBaseUrl${categories[0]}', $rssTrue)," +
+          "('テクノロジー', 'はてなブックマーク/新着', '$hatebuEntryBaseUrl${categories[5]}', $rssTrue)," +
+          "('総合', 'はてなブックマーク/人気', '$hatebuHotBaseUrl${categories[0]}', $rssTrue)," +
+          "('テクノロジー', 'はてなブックマーク/人気', '$hatebuHotBaseUrl${categories[5]}', $rssTrue)," +
+          "('デイリーランキング', 'Qiita', '$qiitaBaseUrl/daily', $rssFalse)," +
+          "('ウィークリーランキング', 'Qiita', '$qiitaBaseUrl/weekly', $rssFalse)," +
+          "('人気ランキング', 'Qiita', '$qiitaBaseUrl/popular', $rssFalse)," +
+          "('コリス', 'Web', 'http://coliss.com/feed/', $rssTrue)," +
+          "('GIGAZINE', '情報全般', 'http://feed.rssad.jp/rss/gigazine/rss_atom', $rssTrue)"
+    }
+  companion object {
+    val TABLE_NAME_GROUPLIST = "grouplist"
+    val TABLE_NAME_URLLIST = "urllist"
+  }
+}
